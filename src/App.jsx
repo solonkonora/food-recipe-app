19562206d440c16de931c7bcfea9183f83633504
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-26 17:29:53 -0800

    adding content

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import { useState } from "react"
-import "./App.css"
-import SearchBar from "./components/SearchBar";
-import RecipeCard from "./components/RecipeCard";
+import { useState } from "react";
+import "./App.css";
+// import SearchBar from "./components/SearchBar";
+// import RecipeCard from "./components/RecipeCard";
 
-const apiUrl = "www.themealdb.com/api/json/v1/1/search.php?s="
+const apiUrl = "www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+const [isLoading, setIsLoading] = useState(false);
+const [query,  setQuery] = useState("");
+const [recipes, setRecipes] = useState([]);
+
+// fetching data
+const seachRecipes = async () => {
+  setIsLoading(true)
+  const url = apiUrl + query;
+  const res = await fetch(url)
+  const data = await res.json()
+
+// console.log(data)
+
+  setQuery(data.meals)
+  setIsLoading(false)
+};
+
+useEffect(() => {
+  seachRecipes()
+}, []);
 
   return (
-    <>
       <div className="container">
-        <h2>My Recipes </h2>
+        <h2>Le Recipees </h2>
+        <div className="recipes">
+        (recipes ? recipes.map((recipe) => (
+          <RecipeCard key={recipe.idMeal} recipe={recipe} />
+        ))
+        : "No Recipes!")
+
+        </div>
      
       </div>
-    </>
-  )
+  );
 }
 
 export default App
--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -1,11 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-const RecipeCard = (props) => {
+// eslint-disable-next-line react/prop-types
+const RecipeCard = ({ recipe }) => {
+   // eslint-disable-next-line react/prop-types, no-unused-vars
+   const { idMeal, strMeal, strCategory, strMealThumb } = recipe;
+
     return (
-        <div>
+        <div className="card">
+            <img
+            src="{strMealThumb}"
+            alt={"strMeal"}
+            className={"card-image"}
+            />
+
+<div className="card-body">
+    <span className="category">{strCategory}</span>
+    <h3>{strMeal}</h3>
+<a href={"https://www.themealdb.com/meal/" + idMeal} target="_blank" rel="noreferrer">Ingredients</a>
+</div>
 
         </div>
     )
-}
-export default RecipeCard
+    };
+export default RecipeCard;
