18066a9135dcaa6283e7153bec75f01fbbb7ad5a
solonkonora <nkwadanora@gmail.com>
2024-03-03 05:08:25 -0500

    formatting code for search bar

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,6 +1,4 @@
-//eslint-disable-next-line no-unused-vars
 import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import { useAppContext } from "../context/AppContext";
 
 const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
@@ -9,11 +7,6 @@ const SearchBar = () => {
   const { searchRecipes } = useAppContext();
 
   const [query, setQuery] = useState("");
-
-  //   const handleSubmit = (e) => {
-  //     // e.preventDefault();
-  //     searchRecipes();
-  //   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -58,42 +51,4 @@ const SearchBar = () => {
   );
 };
 
-
-// SearchBar.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   isLoading: PropTypes.bool.isRequired,
-//   handleSubmit: PropTypes.func.isRequired,
-//   onChange: PropTypes.func.isRequired,
-// };
-
 export default SearchBar;
-
-
-// import React, { useEffect } from "react";
-// import PropTypes from "prop-types";
-
-// const SearchBar = ({ value, handleSubmit, onChange }) => {
-//   useEffect(() => {
-//     handleSubmit();
-//   }, [value, handleSubmit]);
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         value={value}
-//         onChange={onChange}
-//         placeholder="Search Recipes"
-//         className="form-control"
-//       />
-//       <input type="submit" className="btn" value="Search" />
-//     </form>
-//   );
-// };
-
-// SearchBar.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   handleSubmit: PropTypes.func.isRequired,
-//   onChange: PropTypes.func.isRequired,
-// };
-
-// export default SearchBar;
