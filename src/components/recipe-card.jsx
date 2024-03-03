e948d45968e73f8c46909726d210039107a8d08a
solonkonora <nkwadanora@gmail.com>
2024-03-03 05:12:03 -0500

    formatting code for recipe-card

--- src/components/recipe-card.jsx
+++ src/components/recipe-card.jsx
@@ -1,62 +1,3 @@
-// // eslint-disable-next-line no-unused-vars
-// import React from "react";
-// import PropTypes from "prop-types";
-// import { Link } from "react-router-dom";
-
-// const RecipeCard = ({ recipe }) => {
-//   const { strMeal, strMealThumb, idMeal, strCategory } = recipe;
-
-//   return (
-//     <div className="card"
-//     // style={{ height: "500px", width: "500px" }}
-//     >
-//       <img src={strMealThumb} alt={strMeal} className="card-image" />
-
-//       <div className="card-body">
-//         <span className="category">{strCategory}</span>
-//         <h3>{strMeal}</h3>
-//         <Link
-//           to={`/ingredients/${idMeal}`}
-//           style={{ color: "white", textDecoration: "none" }}
-//         >
-//           ingredients
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// RecipeCard.propTypes = {
-//   recipe: PropTypes.shape({
-//     idMeal: PropTypes.string.isRequired,
-//     strMeal: PropTypes.string.isRequired,
-//     strMealThumb: PropTypes.string.isRequired,
-//     strCategory: PropTypes.string.isRequired,
-//   }).isRequired,
-// };
-
-// export default RecipeCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import AddRecipe from "./add-recipe";
@@ -104,9 +45,6 @@ const RecipeCard = ({
     onDeleteRecipe(idMeal);
   };
 
-
-
-  
   return (
     <div className="card">
       <img src={strMealThumb} alt={strMeal} className="card-image" />
