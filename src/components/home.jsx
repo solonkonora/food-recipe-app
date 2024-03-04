209d9f1785ce733e6d1bcacad3791884396a171b
solonkonora <nkwadanora@gmail.com>
2024-03-04 13:35:02 -0500

    make mobile small  screen responsive

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,8 +10,7 @@ return (
 <div className="division-one">
   <div className="section-one">
     <h2 className="section-title">Unleash the full potential of Delicious Recipes</h2>
-    <p className="section-text">Recipe App is website assistant, Lorem ipsum dolor sit amet consectetur adipisicing
-      consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+    <p className="section-text">"Explore a world of culinary delights with our food recipe app, where every dish is a delectable adventure waiting to be discovered."</p>
 
     <div className="button">
       <button className="button-state-app">Get the App &rarr;</button>
@@ -23,18 +22,18 @@ return (
         <h2 className="section-num">4.8</h2>
         <p className="section-texts">Rating on App store</p>
       </div>
-      <div className="num-text">
+      {/* <div className="num-text">
         <h2 className="section-num">700k</h2>
         <p className="section-texts">Active hours</p>
-      </div>
+      </div> */}
     </div>
   </div>
 
   <div className="section-two">
-    <img src="https://media.istockphoto.com/id/1383962772/photo/nigerian-food.jpg?s=612x612&w=0&k=20&c=u-_1qwHBoY9Yh3alnsRbix6hxgl3_TXhT9lpEvlhH10=" alt="example" />
-    <img src="https://media.istockphoto.com/id/1383962772/photo/nigerian-food.jpg?s=612x612&w=0&k=20&c=u-_1qwHBoY9Yh3alnsRbix6hxgl3_TXhT9lpEvlhH10=" alt="example" />
-    {/* <img src="https://media.istockphoto.com/id/1383962772/photo/nigerian-food.jpg?s=612x612&w=0&k=20&c=u-_1qwHBoY9Yh3alnsRbix6hxgl3_TXhT9lpEvlhH10=" alt="example" /> */}
-
+  <div class="image-container">
+  <img src="https://media.istockphoto.com/id/1383962772/photo/nigerian-food.jpg?s=612x612&w=0&k=20&c=u-_1qwHBoY9Yh3alnsRbix6hxgl3_TXhT9lpEvlhH10=" alt="example" />
+  <img src="https://media.istockphoto.com/id/1383962772/photo/nigerian-food.jpg?s=612x612&w=0&k=20&c=u-_1qwHBoY9Yh3alnsRbix6hxgl3_TXhT9lpEvlhH10=" alt="example" />
+</div>
   </div>
 </div>
 </div>
